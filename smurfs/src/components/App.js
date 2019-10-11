dbfdbbcf2704e502da2dbb23da8d5960235555a0
Nicole Hollis <nicole@emmabeauty.co>
2019-10-11 11:05:32 -0700

    MVP in progress

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,21 +1,31 @@
-import React, { createContext, Component } from "react";
+import React, { Component } from "react";
+import {Route} from "react-router-dom";
 import "./App.css";
+import {SmurfContext} from "../contexts/SmurfContext";
+import {VillageContext} from "../contexts/VillageContext";
+import useFetch from "../hooks/useFetch";
+import Smurfs from "./Smurfs";
+import Village from "./Village";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      origsmurfs: origSmurfData,
-    };
-  }
-  render() {
+
+const App = () => {
+
+ const [smurfs, setSmurfData] = useFetch('http://localhost:3333/smurfs');
+      
+  
     return (
+      // <SmurfContext.Provider value={{smurfs}}>
+      // <VillageContext.Provider value={smurfData}>
       <div className="App">
         <h1>Welcome new Smurfs to the Village</h1>
-       
+        <Route exact path="/" component={Smurfs}/>
+        <Route path="/village" component={Village}/>}
+        
       </div>
+      /* </SmurfContext.Provider>
+      </VillageContext.Provider> */
     );
   }
-}
+
 
 export default App;
