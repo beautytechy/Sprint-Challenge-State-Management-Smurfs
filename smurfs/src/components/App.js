3cfb797abc2a2405eb3d5818a6698ecbd6f8743c
Nicole Hollis <nicole@emmabeauty.co>
2019-10-11 09:18:21 -0700

    Questions answered, contexts created

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from "react";
+import React, { createContext, Component } from "react";
 import "./App.css";
 
 class App extends Component {
-
-
+  constructor() {
+    super();
+    this.state = {
+      origsmurfs: origSmurfData,
+    };
+  }
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>Welcome new Smurfs to the Village</h1>
+       
       </div>
     );
   }
